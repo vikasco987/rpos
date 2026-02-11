2d1866e51aeab6504c55ff6db723ec270c7ef38f
rammagicscale-netizen <ram.magicscale@gmal.com>
2026-02-11 12:49:08 +0530

    add clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,21 @@
+// import { clerkMiddleware } from "@clerk/nextjs/server";
+
+// export default clerkMiddleware();
+
+// export const config = {
+//   matcher: [
+//     "/((?!_next|.*\\..*).*)",
+//     "/(api|trpc)(.*)"
+//   ],
+// };
+
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    "/((?!_next|.*\\..*).*)",
-    "/(api|trpc)(.*)"
+    "/((?!_next|static|favicon.ico).*)",
+    "/api/(.*)"
   ],
 };
